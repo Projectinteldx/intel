c642231f95b08b0aca3401a72ccc367aba2ffcd7
ChoeEunho <chldmsgh99@naver.com>
2024-07-26 14:39:05 +0900

    Add files via upload

---
+++ artith.py
@@ -0,0 +1,12 @@
+first = int(input("첫 번째 정수 : "))
+secound = int(input("두 번째 정수: "))
+
+print("{} + {} = {}".format(first, secound, (first + secound)))
+print("{} - {} = {}".format(first, secound, (first - secound)))
+print("{} * {} = {}".format(first, secound, (first * secound)))
+print("{} / {} = {}".format(first, secound, (first / secound)))
+print("{} % {} = {}".format(first, secound, (first % secound)))
+print("{} ** {} = {}".format(first, secound, (first ** secound)))
+
+
+#산술 연산자 : 수와 수를 연산해서 결과가 수렴 연산
